077944549b9f307168825ba048f8927a147456c9
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-16 18:31:39 -0800

    Mod 4 !!!

--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -10,17 +10,29 @@ public class SleepyHead {
 
     public static void main(String[] args) {
         
-        boolean isWeekday, isVacation;
-        
+    
+        boolean isWeekday = false, isVacation = false;
         /* You MUST use the above boolean variables in your code */
         
         /*
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-       JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-        JOptionPane.showConfirmDialog(null, "Are you on vacation?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-        /*
+    int weekday= JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+       int vacation  = JOptionPane.showConfirmDialog(null, "Are you on vacation?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+if(weekday==0) {
+ isWeekday = true;
+}
+if(vacation==0) {
+	isVacation = true;
+}
+if(isWeekday == true && isVacation == true || isWeekday == false) {
+	JOptionPane.showMessageDialog(null, "Sleep in!");
+}
+if (isWeekday == true && isVacation == false) {
+	JOptionPane.showMessageDialog(null, "Get up lazybones!");
+}
+       /*
          * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!�? If it is a weekday, and we are on vacation,
          * print “sleep in�?.
